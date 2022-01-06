4cedc0f1b7e09cf4f03de331798e66ed88c7ac81
Amir Pathan <amir.pathan.180@gmail.com>
2022-01-07 01:04:44 +0530

    a

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+import setuptools
+
+setuptools.setup(
+    name='F12022',  
+    version='0.1',
+    author="ghfgh",
+    author_email="hmjh",
+    description="hjfj",
+    long_description="hjfj",
+    url="https://github.com/MDAmirPathan/F12022",
+    packages=["F12022"],
+    entry_points = {
+        "console_scripts": ['F12022 = F12022.F12022:main']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
